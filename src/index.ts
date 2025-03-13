f9361ed1de37de8af8caec83c9d30157db6af9bb
Clément Murzeau <clement@studiorelief.fr>
2025-03-13 14:17:12 +0100

    build dist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { greetUser } from '$utils/greet';
+import './index.css';
+
+import { navbarHoverButton, navbarHoverLink } from '$utils/component/navbarHover';
+import { svgComponent } from '$utils/global/tricks';
+// import { heroPastilleHover } from '$utils/home/homeHero';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  // icons in component
+  svgComponent();
+
+  // navbar hover
+  navbarHoverLink();
+  navbarHoverButton();
+
+  // hero pastille hover
+  //   heroPastilleHover();
 });
